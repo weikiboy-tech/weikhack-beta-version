ab274b131ffe258ebe96aa15d8ae0eef78c1d399
weikiboy-tech <austriangamer70@gmail.com>
2026-05-16 14:22:54 +0200

    Add fullbright option accessor

---
+++ src/main/java/com/example/weikhack/mixin/SimpleOptionAccessor.java
@@ -0,0 +1,11 @@
+package com.example.weikhack.mixin;
+
+import net.minecraft.client.option.SimpleOption;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(SimpleOption.class)
+public interface SimpleOptionAccessor {
+    @Accessor("value")
+    void weikhack$setValue(Object value);
+}
